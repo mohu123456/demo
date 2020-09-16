a4ebf10043f1f88c4d9aef4bbee727c8ef37f1aa
mohu <799473217@qq.com>
2020-09-16 19:06:27 +0800

    创建了一个类，test02

---
+++ test/src/com/atguigu/Test02.java
@@ -0,0 +1,14 @@
+package com.atguigu;
+
+import java.sql.SQLOutput;
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(12);
+        System.out.println(13);
+        System.out.println(14);
+        System.out.println(15);
+        System.out.println(16);
+    }
+}
