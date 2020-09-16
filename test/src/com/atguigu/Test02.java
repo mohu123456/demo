f3711ddc2b8751b8a3d0bc26813ad728b90c9afc
mohu <799473217@qq.com>
2020-09-16 19:19:55 +0800

    测试冲突，edit by hotfix

--- test/src/com/atguigu/Test02.java
+++ test/src/com/atguigu/Test02.java
@@ -5,7 +5,7 @@ import java.sql.SQLOutput;
 public class Test02 {
     public static void main(String[] args) {
         System.out.println(1);
-        System.out.println(12);
+        System.out.println(12);//edit by hotfix
         System.out.println(13);
         System.out.println(14);
         System.out.println(15);
